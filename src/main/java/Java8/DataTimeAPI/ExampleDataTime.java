cc1d6d762507d70190fb94d84d0fe4183b7b4a09
Evgenii_Lartcev <i.dolende@gmailc.om>
2016-08-11 12:35:02 +0300

    just some features

--- src/main/java/Java8/DataTimeAPI/ExampleDataTime.java
+++ src/main/java/Java8/DataTimeAPI/ExampleDataTime.java
@@ -27,12 +27,22 @@ public class ExampleDataTime {
 
         Duration d2 = Duration.between(instant, instant1);
 
+        final Clock clock = Clock.systemUTC();
+        final LocalDate dateFromClock = LocalDate.now( clock );
+        final LocalDateTime datetimeFromClock = LocalDateTime.now( clock );
+
         //int day1 = instant.get(ChronoField.DAY_OF_WEEK);//Exception in thread "main" java.time.temporal.UnsupportedTemporalTypeException: Unsupported field: DayOfWeek
         //DayOfWeek dayString1 = DayOfWeek.of(day1);
 
         System.out.println(day + "\t" + month1 + "\t" + year1 + "\t" + dayString  + "\t");
-
         System.out.println(instant + "\t" + instant1);
         System.out.println(d2.getSeconds() + "\t" + d2.toNanos() + "\t" + d2.getNano());
+
+        System.out.println( "clock.instant()" + "\t" + clock.instant() );
+        System.out.println( clock.millis() );
+        System.out.println( "clock.instant().getters" + "\t" + clock.instant().isSupported(ChronoField.DAY_OF_WEEK) );
+        System.out.println( dateFromClock );
+        System.out.println( datetimeFromClock);
+
     }
 }
