8df4ebbe4b8f02ba2416583aef04a0b3f6939794
Evgenii_Lartcev <i.dolende@gmailc.om>
2016-08-17 16:13:35 +0300

    timing

--- src/main/java/Java8/StreamAPI/ParallelArrays.java
+++ src/main/java/Java8/StreamAPI/ParallelArrays.java
@@ -11,15 +11,25 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class ParallelArrays {
     public static void main( String[] args ) {
-        long[] arrayOfLong = new long [ 20000 ];
+        long[] arrayOfLong = new long [ 20000000 ];
         Arrays.parallelSetAll( arrayOfLong,
                 index -> ThreadLocalRandom.current().nextInt( 1000000 ) );
         Arrays.stream( arrayOfLong ).limit( 10 ).forEach(
                 i -> System.out.print( i + " " ) );
         System.out.println();
+        final long startTime1 = System.nanoTime();
         Arrays.parallelSort( arrayOfLong );
+        final long endTime1= System.nanoTime();
+
+        final long startTime2 = System.nanoTime();
+        Arrays.sort(arrayOfLong);
+        final long endTime2= System.nanoTime();
+
         Arrays.stream( arrayOfLong ).limit( 10 ).forEach(
                 i -> System.out.print( i + " " ) );
         System.out.println();
+        System.out.println();
+        System.out.println("Arrays.parallelSort timing" + "\t" + ((endTime1-startTime1)/(Math.pow(10, 8))));
+        System.out.println("Arrays.sort timing" + "\t" + ((endTime2-startTime2)/(Math.pow(10, 8))));
     }
 }
