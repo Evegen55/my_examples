cf5902aa97ced50fab5a9991be2f9d234c0d69c1
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-09-12 14:10:08 +0300

    add some tests

--- src/test/java/frameworks_examples/MockitoHelloWorld.java
+++ src/test/java/frameworks_examples/MockitoHelloWorld.java
@@ -2,11 +2,14 @@ package frameworks_examples;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Evgenii_Lartcev on 9/12/2016.
@@ -19,4 +22,38 @@ public class MockitoHelloWorld {
         String result = i.next()+" "+i.next();
         assertEquals("Hello World", result);
     }
+
+    @Test
+    public void with_arguments() {
+        Comparable c = mock(Comparable.class);
+        when(c.compareTo("Test")).thenReturn(1);
+        assertEquals(1, c.compareTo("Test"));
+    }
+
+    @Test
+    public void with_unspecified_arguments() {
+        Comparable c = mock(Comparable.class);
+        when(c.compareTo(anyInt())).thenReturn(-1);
+        assertEquals(-1, c.compareTo(5));
+    }
+
+    @Test(expected=IOException.class)
+    public void OutputStreamWriter_rethrows_an_exception_from_OutputStream()
+            throws IOException {
+        OutputStream mock = mock(OutputStream.class);
+        OutputStreamWriter osw = new OutputStreamWriter(mock);
+        doThrow(new IOException()).when(mock).close();
+        osw.close();
+    }
+
+    @Test
+    public void OutputStreamWriter_Closes_OutputStream_on_Close()
+            throws IOException {
+        OutputStream mock = mock(OutputStream.class);
+        OutputStreamWriter osw = new OutputStreamWriter(mock);
+        osw.close();
+        verify(mock).close();
+    }
+
+
 }
