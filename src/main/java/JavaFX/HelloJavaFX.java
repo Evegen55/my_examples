5066d606d75a5f976a15116ffe76af7d8f8db5c5
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-10-11 00:17:36 +0300

    trying JavaFX

---
+++ src/main/java/JavaFX/HelloJavaFX.java
@@ -0,0 +1,19 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Evegen (create on 10.10.2016).
+ */
+public class HelloJavaFX extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+    //this is no necessary need - try t comment it
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
