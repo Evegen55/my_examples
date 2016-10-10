fa1c46ff0842093d705559ba23231de4a264b8ae
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-10 18:37:14 +0300

    right way to check parenthesis

--- src/main/java/Java7/ADT/CheckParenthesis.java
+++ src/main/java/Java7/ADT/CheckParenthesis.java
@@ -1,5 +1,9 @@
 package Java7.ADT;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,8 +13,41 @@ import java.util.List;
 public class CheckParenthesis {
 
     public static boolean check(String input) {
-        LinkedList<Character> list = new LinkedList<>();
+        final Logger logger = LogManager.getLogger(CheckParenthesis.class.getName());
 
-        return false;
+        LinkedList<Character> list = new LinkedList<>(); //this stack may be with infinity size
+        for (char c : input.toCharArray()) {
+            switch(c) {
+                case '{':
+                case '[':
+                case '(':
+                    list.push(c);
+                    break;
+                case '}':
+                case ']':
+                case ')':
+                    if (!list.isEmpty()) {
+                        char c_out = list.pop();
+                        if ((c == '}' && c_out != '{') ||
+                                ((c == ']' && c_out != '[')) ||
+                                ((c == ')' && c_out != '(')
+                                )) {
+                            logger.error("Error, the parenthesis '" + c + "' with no pair");
+                            return false;
+                        }
+                    } else {
+                        break;
+                    }
+                    default:
+                        break;
+
+            }
+        }
+        if (!list.isEmpty()) {
+            logger.error("the stack is not empty");
+            return false;
+        }
+
+        return true;
     }
 }
--- src/test/java/Java7/ADT/CheckParenthesisTest.java
+++ src/test/java/Java7/ADT/CheckParenthesisTest.java
@@ -8,10 +8,29 @@ import static org.junit.Assert.*;
  * @author Evgenii_Lartcev (created on 10/10/2016).
  */
 public class CheckParenthesisTest {
+
     @Test
-    public void check() throws Exception {
+    public void check1() throws Exception {
         String input1 = "c[d]";
         assertTrue(CheckParenthesis.check(input1));
+    }
+
+    @Test
+    public void check2() throws Exception {
+        String input1 = "c[d";
+        assertFalse(CheckParenthesis.check(input1));
+    }
+
+    @Test
+    public void check3() throws Exception {
+        String input1 = "a{b[c]d}e";
+        assertTrue(CheckParenthesis.check(input1));
+    }
+
+    @Test
+    public void check4() throws Exception {
+        String input1 = "a{b(c]d}e";
+        assertFalse(CheckParenthesis.check(input1));
     }
 
 }
