43efbd048ba714da46c5eabcac60de4cf47f2912
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-17 16:22:26 +0300

    javafx example with switching scenes

--- src/main/java/JavaFX/HelloJavaFXSwitchScenes.java
+++ src/main/java/JavaFX/HelloJavaFXSwitchScenes.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -21,9 +23,24 @@ public class HelloJavaFXSwitchScenes extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-        Label label1 = new Label("Welcome to scene1");
-        Button button1 = new Button("Go to Scene 2");
-        button1.setOnAction(event -> window.setScene(scene2));
-        // TODO: 10/14/2016  
+        Label label_1 = new Label("Welcome to scene1");
+        Button button_1 = new Button("Go to Scene 2");
+        button_1.setOnAction(event -> window.setScene(scene2));
+
+        //layout 1 children are laid out in vertical column
+        VBox layout_1 = new VBox(20);
+        layout_1.getChildren().addAll(label_1, button_1);
+        scene1 = new Scene(layout_1, 200, 200);
+
+        Button button_2 = new Button("Go to Scene 1");
+        button_2.setOnAction(event -> window.setScene(scene1));
+
+        //layout 2
+        StackPane layout_2 = new StackPane();
+        layout_2.getChildren().add(button_2);
+        scene2 = new Scene(layout_2, 600, 600);
+
+        window.setScene(scene2);
+        window.show();
     }
 }
