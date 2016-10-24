8b3258c8d30e1c6335ab4e747365511abc9990f9
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-24 18:09:45 +0300

    todo -implement!

---
+++ src/test/java/Java7/ADT/LinkedList.java
@@ -0,0 +1,57 @@
+package Java7.ADT;
+
+import java.util.Iterator;
+
+/**
+ * @author Evgenii_Lartcev (created on 10/24/2016).
+ */
+public class LinkedList<T> implements Iterable<T> {
+
+    private Node<T> first;
+    private Node<T> last;
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            Node<T> current = first;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+
+    public void add(T t) {
+        Node<T> node = new Node<T>(t, null);
+
+        if (first != null) {
+            first.next = node;
+        }
+
+        if (last != null) {
+            last.next = node;
+        }
+        last = node;
+
+    }
+}
