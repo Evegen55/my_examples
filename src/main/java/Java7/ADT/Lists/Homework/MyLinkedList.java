39963dd97fe32dc8ac64aec1e426cc93a15d54e0
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-31 18:42:32 +0300

    write a LinkedList

---
+++ src/main/java/Java7/ADT/Lists/Homework/MyLinkedList.java
@@ -0,0 +1,295 @@
+package Java7.ADT.Lists.Homework;
+
+import java.util.AbstractList;
+
+
+/**
+ * A class that implements a doubly linked list
+ *
+ * @param <E> The type of the elements stored in the list
+ * @author UC San Diego Intermediate Programming MOOC team
+ */
+public class MyLinkedList<E> extends AbstractList<E> {
+    private LLNode<E> head;
+    private LLNode<E> tail;
+    private int size;
+
+    private int indexOfNode = indexOf(tail);
+
+    /**
+     * Create a new empty LinkedList
+     */
+    public MyLinkedList() {
+        head = new LLNode<E>(null);
+        tail = new LLNode<E>(null);
+        head.setNext(tail);
+        tail.setPrev(head);
+        size = 0;
+    }
+
+    public LLNode<E> getHead() {
+        return head;
+    }
+
+    public void setHead(LLNode<E> head) {
+        this.head = head;
+    }
+
+    public LLNode<E> getTail() {
+        return tail;
+    }
+
+    public void setTail(LLNode<E> tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Appends an element to the end of the list.
+     * It depends on what type od data allows to store in a List
+     *
+     * @param element The element to add
+     */
+    public boolean add(E element) {
+        if (element == null) {
+            throw new NullPointerException("Nulls not allowed");
+        }
+        tail = new LLNode<E>(element, tail);
+        indexOfNode++;
+        tail.setIndexNode(indexOfNode);
+        tail.setData(element);
+        size++;
+        return true;
+    }
+
+    /**
+     * Get the element at position index
+     *
+     * @throws IndexOutOfBoundsException if the index is out of bounds.
+     */
+    public E get(int index) {
+        LLNode<E> searchNode = recFindIndex(index, tail);
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Warning! You've just tried to get an object from empty list");
+        }
+        if (index < 0 && index > size) {
+            throw new IndexOutOfBoundsException("Warning! You've just tried to get an object out of bounds");
+        }
+        return searchNode.getData();
+    }
+
+
+    /**
+     * Add an element to the list at the specified index
+     *
+     * @param index   where the element should be added
+     * @param element The element to add
+     */
+    public void add(int index, E element) {
+        if (index < tail.getIndexNode()) {
+            LLNode<E> searched = recFindIndex(index, this.tail);
+            LLNode<E> beforeSearched = recFindIndex(index, this.tail).getPrev();
+            LLNode<E> pastedNode = new LLNode<E>(element, beforeSearched);
+            pastedNode.setIndexNode(index);
+            pastedNode.setPrev(beforeSearched);
+            pastedNode.setNext(searched);
+            beforeSearched.setNext(pastedNode);
+            searched.setPrev(pastedNode);
+            size++;
+            recAddIndexes(index, tail);
+        } else if (index < 0 && index > size) {
+            throw new IndexOutOfBoundsException("Warning! You've just tried to add an object out of bounds");
+        } else {
+            add(element);
+        }
+    }
+
+    /**
+     * Remove a node at the specified index and return its data element.
+     *
+     * @param index The index of the element to remove
+     * @return The data element removed
+     * @throws IndexOutOfBoundsException If index is outside the bounds of the list
+     */
+    public E remove(int index) {
+        recDecrIndexes(index + 1, tail);
+        LLNode<E> deletedNode = recFindIndex(index, this.tail).getPrev();
+        E deletedData = deletedNode.getData();
+        LLNode<E> deletedNodeNext = deletedNode.getNext();
+        LLNode<E> deletedNodePrev = deletedNode.getPrev();
+        deletedNodeNext.setPrev(deletedNodePrev);
+        deletedNodePrev.setNext(deletedNodeNext);
+        size--;
+        return deletedData;
+    }
+
+    /**
+     * Set an index position in the list to a new element
+     *
+     * @param index   The index of the element to change
+     * @param element The new element
+     * @return The element that was replaced
+     * @throws IndexOutOfBoundsException if the index is out of bounds.
+     */
+    public E set(int index, E element) {
+        E deletedData;
+        if (element == null) {
+            throw new NullPointerException("Nulls not allowed");
+        } else if (index < 0 && index > size) {
+            throw new IndexOutOfBoundsException("Warning! You've just tried to add an object out of bounds");
+        } else {
+            LLNode<E> settedNode = recFindIndex(index, this.tail);
+            deletedData = settedNode.getData();
+            settedNode.setData(element);
+        }
+        return deletedData;
+    }
+
+    //helper method for searching by index - search
+    protected LLNode<E> recFindIndex(final int searchIndex, final LLNode<E> tailSearch) {
+        if (searchIndex < 0 || searchIndex > tailSearch.getIndexNode() || tailSearch == null) {
+            throw new IndexOutOfBoundsException("Warning! You've just tried to find null object");
+        } else if (searchIndex == tailSearch.getIndexNode()) {
+            return tailSearch;
+        } else {
+            return recFindIndex(searchIndex, tailSearch.getPrev());
+        }
+    }
+
+    //helper method for addition indexes after addition an element at index
+    private void recAddIndexes(final int edgeLeftIndex, final LLNode<E> tailSearch) {
+        for (int i = tailSearch.getIndexNode(); i >= edgeLeftIndex; i--) {
+            recFindIndex(i, tailSearch).incrementIndex();
+        }
+    }
+
+    //helper method for addition indexes after addition an element at index
+    private void recDecrIndexes(int edgeLeftIndex, LLNode<E> tailSearch) {
+        for (int i = 0; i <= tailSearch.getIndexNode() - edgeLeftIndex; i++) {
+            recFindIndex(edgeLeftIndex + i, tailSearch).decrementIndex();
+        }
+    }
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class LLNode<E> {
+    private LLNode<E> prev;
+    private LLNode<E> next;
+    private E data;
+    private int indexNode;
+
+    public LLNode() {
+        prev = null;
+        next = null;
+    }
+
+    public LLNode(E e) {
+        this();
+        data = e;
+    }
+
+    public LLNode(E e, LLNode<E> prevNode) {
+        data = e;
+        //for SingleLinkedList
+        this.next = prevNode.next;
+        prevNode.next = this;
+        //for DoublyLinkedList
+        this.prev = prevNode;
+
+    }
+
+    public LLNode<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(LLNode<E> prev) {
+        this.prev = prev;
+    }
+
+    public LLNode<E> getNext() {
+        return next;
+    }
+
+    public void setNext(LLNode<E> next) {
+        this.next = next;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public int getIndexNode() {
+        return indexNode;
+    }
+
+    public void setIndexNode(int indexNode) {
+        this.indexNode = indexNode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LLNode<?> llNode = (LLNode<?>) o;
+
+        if (indexNode != llNode.indexNode) return false;
+        return getData() != null ? getData().equals(llNode.getData()) : llNode.getData() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getData() != null ? getData().hashCode() : 0;
+        result = 31 * result + indexNode;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String ret = null;
+        if (prev != null & next != null) {
+            ret = "LLNode{" +
+                    "prev data = " + prev.getData() +
+                    ", next data = " + next.getData() +
+                    ", data = " + data +
+                    ", indexNode = " + indexNode +
+                    '}';
+        } else if (prev == null) {
+            ret = "LLNode{" +
+                    "prev data = NULL" +
+                    ", next data = " + next.getData() +
+                    ", data = " + data +
+                    ", indexNode = " + indexNode +
+                    '}';
+        }else if (next == null) {
+            ret = "LLNode{" +
+                    "prev data = " + prev.getData() +
+                    ", next data = NULL" +
+                    ", data =" + data +
+                    ", indexNode = " + indexNode +
+                    '}';
+        }
+        return ret;
+    }
+
+    void incrementIndex() {
+        indexNode = indexNode + 1;
+    }
+
+    void decrementIndex() {
+        indexNode = indexNode - 1;
+    }
+}
+
+// TODO: 10/31/2016 implement iterator
---
+++ src/test/java/Java7/ADT/Lists/Homework/MeOwnTest.java
@@ -0,0 +1,42 @@
+package Java7.ADT.Lists.Homework;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class MeOwnTest {
+
+    MyLinkedList<String> myOwnList;
+
+    @Before
+    public void setUp() {
+        myOwnList = new MyLinkedList<>();
+        myOwnList.add("String#0");
+        myOwnList.add("String#1");
+        myOwnList.add("String#2");
+        myOwnList.add("String#3");
+        myOwnList.add("String#4");
+    }
+
+    @Test
+    public void testSizeAndIndex() {
+        int indexOfTail = myOwnList.getTail().getIndexNode();
+        int indexOfHead = myOwnList.getHead().getIndexNode();
+        assertTrue(indexOfHead == 0);
+        assertTrue(indexOfTail == 4);
+        assertTrue(myOwnList.size() == 5);
+        System.out.print("content of tail" + "\t" + myOwnList.getTail());
+    }
+
+    @Test
+    public void testRecFindIndex() {
+        for (int i = 0; i < myOwnList.size(); i++) {
+            assertThat(myOwnList.recFindIndex(i, myOwnList.getTail()).getData(), is("String#" + i));
+            assertTrue(myOwnList.recFindIndex(i, myOwnList.getTail()).getIndexNode() == i);
+        }
+    }
+
+}
