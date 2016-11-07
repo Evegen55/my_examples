1fd273c1434ccabb09d0c9409c68d8f659c834e8
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-11-07 16:52:54 +0300

    success

--- src/main/java/Algorithms/SuccessorOfX.java
+++ src/main/java/Algorithms/SuccessorOfX.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 /**
  * Created by Evegen on 06.11.2016.
+ * Under course taken on a www.coursera.org/learn/introduction-to-algorithms
  */
 public class SuccessorOfX {
     private final int[] mainArray;
@@ -11,7 +12,8 @@ public class SuccessorOfX {
     public SuccessorOfX(int size) {
         mainArray = new int[size];
         for (int i = 0; i < size; i++) {
-            mainArray[i] = new Random(100).nextInt();
+            //mainArray[i] = new Random(100).nextInt();
+            mainArray[i] = i;
         }
     }
 
@@ -21,5 +23,26 @@ public class SuccessorOfX {
 
     public int[] getMainArray() {
         return mainArray;
+    }
+
+    /**
+     * Successor with delete. Given a set of N integers S={0,1,...,N−1} and a sequence of requests of the following form:
+     * Remove x from S
+     * Find the successor of x: the smallest y in S such that y>=x.
+     * design a data type so that all operations (except construction) should take logarithmic time or better.
+     *
+     * @param x
+     * @return
+     */
+    public int findSuccessor(int x) {
+        int findedMin = mainArray[0];
+
+        for (int i = 0; i < mainArray.length; i++) {
+            if (mainArray[i] == x) return x;
+            if ((mainArray[i] > x) && mainArray[i] < findedMin) {
+                findedMin = mainArray[i];
+            }
+        }
+        return findedMin;
     }
 }
--- src/test/java/Algorithms/SuccessorOfXTest.java
+++ src/test/java/Algorithms/SuccessorOfXTest.java
@@ -1,7 +1,9 @@
 package Algorithms;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
@@ -10,10 +12,20 @@ import static org.junit.Assert.*;
  * Created by Evegen on 06.11.2016.
  */
 public class SuccessorOfXTest {
+
+    private final int[] arrBeforeSort = {77, 99, 44, 55, 22, 88, 11};
+
     @Test
+    @Ignore
     public void getMainArray() throws Exception {
         SuccessorOfX successorOfX = new SuccessorOfX(10);
-        Stream.of(successorOfX).forEach(System.out::println);
+        System.out.println(Arrays.toString(successorOfX.getMainArray()));
+    }
+
+    @Test
+    public void test1() throws Exception {
+        SuccessorOfX successorOfX = new SuccessorOfX(arrBeforeSort);
+        assertTrue(successorOfX.findSuccessor(31) == 44);
     }
 
 }
