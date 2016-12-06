6dfba20c560dd2613ac77a4dff0575740417dc89
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-12-06 14:10:51 +0300

    examples

--- src/main/groovy/HelloWorld.groovy
+++ src/main/groovy/HelloWorld.groovy
@@ -2,3 +2,37 @@
  * @author Evgenii_Lartcev (created on 12/6/2016).
  */
 println 'Hello from Groovy'
+
+def name='Lartsev'
+
+def prt(q) {
+    if (q == 1) {
+        println 'the if part'
+    } else {
+        println 'the else part'
+    }
+}
+
+println "Hello $name!"
+
+println 6.multiply(11).minus(2)
+
+println 6.multiply(11.minus(2))
+
+println (!false || true) && (false || true)
+
+println  "Hello".size()
+
+println (['H', 'e', 'l', 'l', 'o'].asBoolean())
+
+println (['H', 'e', 'l', 'l', 'o'].reverse())
+
+println (['H', 'e', 'l', 'l', 'o'].last())
+
+println (['H', 'e', 'l', 'l', 'o'].tail())
+
+println (['H', 'e', 'l', 'l', 'o'].head())
+
+println "1 + 2 == ${1 + 2}"
+
+prt(7)
