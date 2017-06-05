63c58a6869c6ef511e8cadcb572385117ef554eb
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-06-05 15:06:03 +0300

    add example how-to publish message in a topic

--- src/main/java/GoogleApiTests/PubSubQuickStart.java
+++ src/main/java/GoogleApiTests/PubSubQuickStart.java
@@ -1,11 +1,18 @@
 package GoogleApiTests;
 
+import com.google.api.core.ApiFuture;
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.api.gax.grpc.InstantiatingChannelProvider;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.pubsub.spi.v1.Publisher;
 import com.google.cloud.pubsub.spi.v1.TopicAdminClient;
 import com.google.cloud.pubsub.spi.v1.TopicAdminSettings;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author (created on 5/31/2017).
@@ -43,6 +50,9 @@ public class PubSubQuickStart {
             System.out.printf("Topic %s:%s created.\n", topic.getProject(), topic.getTopic(), topic);
         }
 
+        //publish message in a topic
+        final ApiFuture<String> stringApiFuture = publishMessageInTopic(topicAdminSettings, topic, "MESSAGE");
+
 
         //delete topic
         try (TopicAdminClient topicAdminClient = TopicAdminClient.create(topicAdminSettings)) {
@@ -51,6 +61,39 @@ public class PubSubQuickStart {
         }
 
 
+    }
+
+    private static ApiFuture<String> publishMessageInTopic(final TopicAdminSettings topicAdminSettings, TopicName topic, String message) throws ExecutionException, InterruptedException {
+        Publisher publisher = null;
+        ApiFuture<String> messageIdFuture = null;
+        try {
+            publisher = Publisher.defaultBuilder(topic)
+                    .setChannelProvider(topicAdminSettings.getChannelProvider())
+                    //.setExecutorProvider(topicAdminSettings.getExecutorProvider())
+                    .build();
+            ByteString data = ByteString.copyFromUtf8(message);
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+            messageIdFuture = publisher.publish(pubsubMessage);
+
+            System.out.println(messageIdFuture.isDone());
+            System.out.println(messageIdFuture.get());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (publisher != null) {
+                try {
+                    publisher.shutdown();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println(messageIdFuture.isDone());
+        System.out.println(messageIdFuture.get());
+
+        return messageIdFuture;
     }
 
 }
