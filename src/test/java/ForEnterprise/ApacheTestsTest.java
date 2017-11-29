788a58a5db9161521b966be3ae2c6573365d9d69
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-11-29 14:56:24 +0300

    read properties

---
+++ src/test/java/ForEnterprise/ApacheTestsTest.java
@@ -0,0 +1,14 @@
+package ForEnterprise;
+
+import org.junit.Test;
+
+/**
+ * @author (created on 11/29/2017).
+ */
+public class ApacheTestsTest {
+    @Test
+    public void me() throws Exception {
+        ApacheTests.readProperty();
+    }
+
+}
