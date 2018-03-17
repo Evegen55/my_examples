ec93ef64290a985980995c719fe4bc9c2e3810d0
Evgenii Lartcev <i.dolende@gmail.com>
2018-03-17 23:29:38 +0300

    annotation processor

---
+++ src/main/java/annotations/ProcessAnnotation.java
@@ -0,0 +1,20 @@
+package annotations;
+
+import java.lang.reflect.Constructor;
+
+public class ProcessAnnotation {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        final Class cl = Class.forName("annotations.AnnotatedClass");
+
+        final Constructor[] constructors = cl.getConstructors();
+        for (final Constructor c : constructors) {
+            if (c.isAnnotationPresent(ClassPreamble.class)) {
+                System.out.println("Annotation for next constructor: " + c);
+            } else {
+                System.err.println("There is no annotations for for next constructor: " + c);
+            }
+        }
+    }
+
+}
