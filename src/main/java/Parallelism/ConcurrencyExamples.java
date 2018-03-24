76b1e59bebd1316796b638a269e47bfd913c0497
Evgenii Lartcev <i.dolende@gmail.com>
2018-03-25 00:36:22 +0300

    example

---
+++ src/main/java/Parallelism/ConcurrencyExamples.java
@@ -0,0 +1,48 @@
+package Parallelism;
+
+import java.util.*;
+
+/**
+ * https://docs.oracle.com/javase/tutorial/collections/streams/parallelism.html
+ */
+public class ConcurrencyExamples {
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8 };
+        List<Integer> listOfIntegers =
+                new ArrayList<>(Arrays.asList(intArray));
+
+        System.out.println("listOfIntegers:");
+        listOfIntegers
+                .stream()
+                .forEach(e -> System.out.print(e + " "));
+        System.out.println("");
+
+        System.out.println("listOfIntegers sorted in reverse order:");
+        Comparator<Integer> normal = Integer::compare;
+        Comparator<Integer> reversed = normal.reversed();
+        Collections.sort(listOfIntegers, reversed);
+        listOfIntegers
+                .stream()
+                .forEach(e -> System.out.print(e + " "));
+        System.out.println("");
+
+        System.out.println("Parallel stream");
+        listOfIntegers
+                .parallelStream()
+                .forEach(e -> System.out.print(e + " "));
+        System.out.println("");
+
+        System.out.println("Another parallel stream:");
+        listOfIntegers
+                .parallelStream()
+                .forEach(e -> System.out.print(e + " "));
+        System.out.println("");
+
+        System.out.println("With forEachOrdered:");
+        listOfIntegers
+                .parallelStream()
+                .forEachOrdered(e -> System.out.print(e + " "));
+        System.out.println("");
+    }
+}
