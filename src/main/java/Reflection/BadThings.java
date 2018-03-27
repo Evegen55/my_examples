3acbcdd718127926d0e93c8ce983f64830ed1c42
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-27 15:27:11 +0300

    set private field by using reflection

--- src/main/java/Reflection/BadThings.java
+++ src/main/java/Reflection/BadThings.java
@@ -6,14 +6,24 @@ public class BadThings {
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         String s = "ABC";
+        String s1 = "abc";
         System.out.println(s.hashCode());
+        System.out.println(s1.hashCode());
 
         Field hashField = s.getClass().getDeclaredField("hash");
-        hashField.setAccessible(true);
         System.out.println(hashField);
 
-        hashField.set(s, 123456);
-
+        hashField.setAccessible(true);
+        hashField.set(s, s1.hashCode());
         System.out.println(s.hashCode());
+        System.out.println(s.equals(s1));
+
+        System.out.println(s + "\t" + s1);
+        Field valueField = s.getClass().getDeclaredField("value");
+        System.out.println(valueField);
+        valueField.setAccessible(true);
+        valueField.set(s, s1.toCharArray());
+        System.out.println(s.equals(s1));
+        System.out.println(s + "\t" + s1);
     }
 }
