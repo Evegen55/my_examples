52fe8def477793ce2f82697ab6c2f0ceeb19f87a
Evgenii Lartcev <i.dolende@gmail.com>
2018-09-30 16:09:45 +0300

    http://minborgsjavapot.blogspot.com/2018/09/debugging-java-streams-with-intellij.html

--- src/main/java/java10/Examples.java
+++ src/main/java/java10/Examples.java
@@ -1,7 +1,10 @@
 package java10;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Examples {
     public static void main(String[] args) {
@@ -9,6 +12,10 @@ public class Examples {
         //https://4comprehension.com/java-immutable-unmodifiable-stream-api-collectors/
         var collect = Stream.of(42)
                 .collect(Collectors.toUnmodifiableList());
+
+        List<String> strings = Stream.of("C", "A", "B")
+                .sorted()
+                .collect(toList());
 
     }
 }
