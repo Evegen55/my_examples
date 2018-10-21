1831925140259863de40b82cb5090ac76e56af41
Evgenii Lartcev <i.dolende@gmail.com>
2018-10-21 22:29:17 +0300

    yet another RxJava2 example

--- src/test/java/itrx/RxJavaExamples.java
+++ src/test/java/itrx/RxJavaExamples.java
@@ -1,6 +1,7 @@
 package itrx;
 
 import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
 
 public class RxJavaExamples {
@@ -10,4 +11,18 @@ public class RxJavaExamples {
         Flowable.just("Hello world")
                 .subscribe(System.out::println);
     }
+
+    @Test
+    public void test1() throws InterruptedException {
+        Flowable.fromCallable(() -> {
+            Thread.sleep(1000); //  imitate expensive computation
+            return "Done";
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.single())
+                .subscribe(System.out::println, Throwable::printStackTrace);
+
+        Thread.sleep(2000); // <--- wait for the flow to finish
+    }
+
 }
