3176507d5f424c8993a1785143340b6df0cca5ca
Evgenii Lartcev <i.dolende@gmail.com>
2020-05-03 21:27:57 +0300

    code up-to-date

---
+++ src/main/java/Algorithms/MathExamples.java
@@ -0,0 +1,12 @@
+package Algorithms;
+
+public class MathExamples {
+
+    public static void main(String[] args) {
+
+        double pow = Math.pow(1.7e+300, 1.7e+300);
+        System.out.println(pow);
+
+        System.out.println(1.7e+3);
+    }
+}
