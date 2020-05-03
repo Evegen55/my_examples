838f4f51d35817c1a274bf5f5d67da71a74f03eb
Evgenii Lartcev <i.dolende@gmail.com>
2020-05-03 21:35:28 +0300

    knowledge refreshed

--- src/main/java/Algorithms/MathExamples.java
+++ src/main/java/Algorithms/MathExamples.java
@@ -4,9 +4,13 @@ public class MathExamples {
 
     public static void main(String[] args) {
 
-        double pow = Math.pow(1.7e+300, 1.7e+300);
-        System.out.println(pow);
+        double pow1 = Math.pow(1.7e+154, 2); //Infinity
+        double pow2 = Math.pow(1.7e+153, 2); //2.8899999999999993E306
+        System.out.println(pow1);
+        System.out.println(pow2);
 
-        System.out.println(1.7e+3);
+        double pow3 = Math.pow(2e+2, 2);  //40000.0
+        System.out.println(pow3);
+        System.out.println(1.7e+3); //1700.0
     }
 }
